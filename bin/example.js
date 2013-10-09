bb8690916d19d3516dfca725bb7857ad983ff916
Sérgio Ramos <mail@sergioramos.me>
2013-10-09 15:25:45 +0100

    improve example readability

--- bin/example.js
+++ bin/example.js
@@ -1,22 +1,22 @@
-var ase = require('../'),
-    argv = require('optimist').argv
+var argv = require('optimist').argv,
+    ase = require('../')
+
 
 var seeds = argv.seeds ? argv.seeds.split(/\,/) : []
 
-ase(seeds, function (e, peer) {
+var peer = ase(seeds, function (e) {
   if(e) throw e
-  
   console.log('%s started', peer.name)
-  
-  peer.on('new', function (peer) {
-    console.log('Found: %s', peer.name())
-  })
-  
-  peer.on('alive', function (peer) {
-    console.log('Alive: %s', peer.name())
-  })
-  
-  peer.on('failed', function (peer) {
-    console.log('Died: %s', peer.name())
-  })
+})
+
+peer.on('new', function (other) {
+  console.log('Found: %s', other.name())
+})
+
+peer.on('alive', function (other) {
+  console.log('Alive: %s', other.name())
+})
+
+peer.on('failed', function (other) {
+  console.log('Died: %s', other.name())
 })
