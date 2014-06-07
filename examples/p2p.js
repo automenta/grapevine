53d72f81da2551e98be13129d699555ce39a8899
sseehh <1s1e1h1@gmail.com>
2014-06-07 14:06:13 -0400

    high-performance UDP messaging feature

---
+++ examples/p2p.js
@@ -0,0 +1,40 @@
+//Run with:
+//	DEBUG=* node examples/p2p.js
+
+var Gossiper = require('../lib/gossiper.js').Gossiper;
+// Create a seed peer.
+var seed = new Gossiper(9000, []);
+seed.start();
+
+var startPort = 9001;
+var numSeeds = 4;
+// Create 20 new peers and point them at the seed (usually this would happen in 20 separate processes)
+// To prevent having a single point of failure you would probably have multiple seeds
+for(var i = 0; i < numSeeds ;i++) {
+  //For IPv6 peers use the format [ad:dre::ss]:port. e.g. [::1]:9000
+
+  var g = new Gossiper(i+startPort+1, 
+		['127.0.0.1:' + (startPort+i+2)]
+  );
+  g.start();
+
+  g.on('update', function(peer, k, v) {
+    console.log("peer " + peer + " set " + k + " to " + v); // peer 127.0.0.1:9999 set somekey to somevalue
+  });
+}
+
+// Add another peer which updates it's state after 15 seconds
+var updater = new Gossiper(startPort, ['127.0.0.1:' + (startPort+1)]);
+updater.start();
+setTimeout(function() {
+  updater.setLocalState('somekey', 'somevalue');
+
+  // with ttl
+  // 10 seconds from now this key will start to expire in the gossip net
+  updater.setLocalState('somekey2', 'somevalue', Date.now() + 10000); 
+}, 1000);
+
+
+seed.on('*', function(d) {
+	console.log('seed', d);
+});
